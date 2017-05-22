ef1032c2c68f77734be6c29431558e7a1344f58a
Tprezioso <tommyprezioso@gmail.com>
2017-05-22 12:40:54 -0400

    first commit

---
+++ index.ios.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {View, Text, AppRegistry, StyleSheet} from 'react-native';
+
+var StopWatch = React.createClass({
+  render: function() {
+    return <View>
+      <Text>
+        00:00.00
+      </Text>
+      {this.startStopButton()}
+      {this.lapButton()}
+    </View>
+  },
+  startStopButton: function (){
+    return <View>
+        <Text>
+          Start
+        </Text>
+      </View>
+  },
+  lapButton: function(){
+    return <View>
+        <Text>
+          Lap
+        </Text>
+      </View>
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
